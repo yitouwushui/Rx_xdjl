95127d3b377071da5bd8c560ba4319eb04248b48
dzx <280611948@qq.com>
2018-02-07 17:29:16 +0800

    修改3

---
+++ app/src/main/java/cn/ecar/insurance/dao/bean/Insurance.java
@@ -0,0 +1,8 @@
+package cn.ecar.insurance.dao.bean;
+
+/**
+ * Created by ding on 2018/2/7.
+ */
+
+public class Insurance extends BaseBean {
+}
---
+++ app/src/main/java/cn/ecar/insurance/dao/gson/InsuranceGson.java
@@ -0,0 +1,11 @@
+package cn.ecar.insurance.dao.gson;
+
+/**
+ * Created by ding on 2018/2/7.
+ */
+
+public class InsuranceGson {
+
+
+
+}
